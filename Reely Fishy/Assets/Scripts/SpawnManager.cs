ca589ca411dfd2e6d022f3b40cfe196622fafdf3
Andresav776 <715867@student.dusd.net>
2023-04-06 11:41:00 -0700

    Added random fish spawning, and object interact satisfactorally

---
+++ Reely Fishy/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] fishyPrefabs;
+    private float zRange = 12;
+    private float rightRangeX = 38;
+    private float leftRangeX = -37;
+
+    private float spawnInterval;
+    private float startDelay = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnInterval = Random.Range(10, 15);
+        InvokeRepeating("SpawnRandomFish", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRandomFish()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(leftRangeX, rightRangeX), 1, Random.Range(-zRange, zRange));
+        int fishIndex = Random.Range(0, fishyPrefabs.Length);
+        Instantiate(fishyPrefabs[fishIndex], spawnPos, fishyPrefabs[fishIndex].transform.rotation);
+    }
+}

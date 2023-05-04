908584c8c5904b0039da116d18528589262a0aa8
Andresav776 <715867@student.dusd.net>
2023-05-04 11:40:27 -0700

    Assets replaced (fix particles)
    Need to play the particle in the place where a fish spawns.

--- Reely Fishy/Assets/Scripts/SpawnManager.cs
+++ Reely Fishy/Assets/Scripts/SpawnManager.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] fishyPrefabs;
+    public ParticleSystem splashParticle;
     private float zRange = 12;
     private float rightRangeX = 38;
     private float leftRangeX = -37;
 
     private float spawnInterval;
-    private float startDelay = 0.1f;
+    private float startDelay = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        spawnInterval = Random.Range(10, 15);
+        spawnInterval = Random.Range(7, 10);
         InvokeRepeating("SpawnRandomFish", startDelay, spawnInterval);
     }
 
@@ -30,5 +31,7 @@ public class SpawnManager : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(leftRangeX, rightRangeX), 1, Random.Range(-zRange, zRange));
         int fishIndex = Random.Range(0, fishyPrefabs.Length);
         Instantiate(fishyPrefabs[fishIndex], spawnPos, fishyPrefabs[fishIndex].transform.rotation);
+        Instantiate(splashParticle, spawnPos, splashParticle.transform.rotation);
+        splashParticle.Play();
     }
 }
